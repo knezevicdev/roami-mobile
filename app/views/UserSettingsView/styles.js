7065c351816b045824cdddc4ec20df8ffddd8250
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2019-03-29 13:21:34 +0100

    [FEATURE_SEARCH] Added search page, settings navigation, fixed styles and bugs

--- app/views/UserSettingsView/styles.js
+++ app/views/UserSettingsView/styles.js
@@ -4,36 +4,25 @@ import { colors } from "../../config";
 export default StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: 40
     },
     text: {
         alignSelf: 'center',
-        color: colors.WHITE
-    },
-    logoContainer: {
-        flex: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    labelContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0
-    },
-    button: {
-        backgroundColor: colors.BUTTON_TRANSPARENT_COLOR,
-        marginHorizontal: 30,
-        alignItems: 'center',
-        justifyContent: 'center',
+        textAlign: 'center',
+        color: colors.WHITE,
+        backgroundColor: colors.BLUE_DARK,
         paddingVertical: 10,
-        borderRadius: 10,
-        marginBottom: 5
+        borderRadius: 4,
+        marginBottom: 5,
+        width: '80%',
     },
     input: {
         backgroundColor: colors.WHITE_TRANSPARENT,
         borderRadius: 10,
         paddingVertical: 10,
         marginHorizontal: 30,
-        marginBottom: 5
+        marginBottom: 5,
+        backgroundColor: colors.BLUE_SEARCH,
+        color: colors.WHITE
     },
 });
