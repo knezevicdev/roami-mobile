82f5afae4fc0fc4b48bcfba29502f7bb82e23d15
Gligorije <gligorije.nikolic989@gmail.com>
2019-04-07 15:00:33 +0200

    Api url update

--- app/config/api.js
+++ app/config/api.js
@@ -1,5 +1,5 @@
 export default { 
-    API_URL: 'http://192.168.0.36:8000/api',
+    API_URL: 'http://roamiapi.test.decem.co/api',
     GOOGLE_IOS_CLIENT_ID: '358499972961-f0dcer0p1bqu6bgn0o4o9llsullq0faf.apps.googleusercontent.com',
     GOOGLE_WEB_CLIENT_ID: '358499972961-nfgn8iml44mf6kk44a21956un4e7i1q6.apps.googleusercontent.com'
 }
